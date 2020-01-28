90499d103e55af871d381034f09f7e4b8ab6e89f
Denis MUraveiko <mr.muraveiko@gmail.com>
2020-01-29 01:42:19 +0300

    move out images storing

--- storage/face_recognition_settings.py
+++ storage/face_recognition_settings.py
@@ -1,26 +1,26 @@
-import face_recognition, cv2, os, numpy as np, time
+import face_recognition
+import cv2
+import os
+import numpy as np
+import time
+
 
 def add_picture_to_dataset(dataset: list, image: np.array, filename: str):
-  was_already_saved = False
-  for saved_image in dataset:
-    if type(saved_image[0] == image) != bool and (saved_image[0] == image).all():
-      was_already_saved = True
-      break
+    was_already_saved = False
+    for [saved_image, _], [_, _] in dataset:
+        if type(saved_image == image) != bool and (saved_image == image).all():
+            was_already_saved = True
+            break
 
-  if not was_already_saved:
-    face_locations, face_encodings = get_face_embeddings_from_image(
-      image, 
-      convert_to_rgb=True)
-  
-    dataset.append([[image, filename], [face_locations, face_encodings]])
-  
-def make_precalculate_work_on_images(images: list, filenames: list) -> list:
-  images_and_vectors = []
+    if was_already_saved:
+        return False
+    else:
+        face_locations, face_encodings = get_face_embeddings_from_image(
+            image,
+            convert_to_rgb=True)
 
-  for i in range(len(images)):
-    add_picture_to_dataset(images_and_vectors, images[i], filenames[i])
-  
-  return images_and_vectors
+        dataset.append([[image, filename], [face_locations, face_encodings]])
+        return True
 
 
 def get_face_embeddings_from_image(image, convert_to_rgb=False):
@@ -39,41 +39,43 @@ def get_face_embeddings_from_image(image, convert_to_rgb=False):
 
     return face_locations, face_encodings
 
-def python_work(images, face_encodings_target, files, length=10):
-  exclude_indexes = []
-  ten_nearest_images_with_coefs = {}
 
-  while len(ten_nearest_images_with_coefs) < length:
-    i = 0
-    minimum_image, filename_of_minimum_image = images[i][0]
-    face_locations_minimum, face_encodings_minimum = images[i][1]
-    
-    while (len(face_locations_minimum) == 0 or i in exclude_indexes) and i < length:
-      i += 1
-      minimum_image, filename_of_minimum_image = images[i][0]
-      face_locations_minimum, face_encodings_minimum = images[i][1]
+def python_work(images, face_encodings_target, length=10):
+    exclude_indexes = []
+    ten_nearest_images_with_coefs = {}
 
-    if i == len(images):
-      break
-    minimum_distance = face_recognition.face_distance(face_encodings_minimum, 
-                                                      face_encodings_target[0])[0]
-    
-    exclude_index = i
-    for j in range(len(images)):
-      image = images[j][0][0]
-      face_locations_current, face_encodings_current = images[j][1]
-      
-      if len(face_locations_current) == 0:
-        continue
-      current_distance = face_recognition.face_distance(face_encodings_current, 
-                                                        face_encodings_target[0])[0]
-      if current_distance < minimum_distance and j not in exclude_indexes:
-        minimum_distance = current_distance
-        minimum_image = image
-        filename_of_minimum_image = images[j][0][1]
-        exclude_index = j
-    
-    ten_nearest_images_with_coefs.update({filename_of_minimum_image : minimum_distance})
-    exclude_indexes.append(exclude_index)
+    while len(ten_nearest_images_with_coefs) < length:
+        i = 0
+        minimum_image, filename_of_minimum_image = images[i][0]
+        face_locations_minimum, face_encodings_minimum = images[i][1]
 
-  return ten_nearest_images_with_coefs
+        while (len(face_locations_minimum) == 0 or i in exclude_indexes) and i < length:
+            i += 1
+            minimum_image, filename_of_minimum_image = images[i][0]
+            face_locations_minimum, face_encodings_minimum = images[i][1]
+
+        if i == len(images):
+            break
+        minimum_distance = face_recognition.face_distance(face_encodings_minimum,
+                                                          face_encodings_target[0])[0]
+
+        exclude_index = i
+        for j in range(len(images)):
+            image = images[j][0][0]
+            face_locations_current, face_encodings_current = images[j][1]
+
+            if len(face_locations_current) == 0:
+                continue
+            current_distance = face_recognition.face_distance(face_encodings_current,
+                                                              face_encodings_target[0])[0]
+            if current_distance < minimum_distance and j not in exclude_indexes:
+                minimum_distance = current_distance
+                minimum_image = image
+                filename_of_minimum_image = images[j][0][1]
+                exclude_index = j
+
+        ten_nearest_images_with_coefs.update(
+            {filename_of_minimum_image: minimum_distance})
+        exclude_indexes.append(exclude_index)
+
+    return ten_nearest_images_with_coefs
--- storage/views.py
+++ storage/views.py
@@ -6,7 +6,9 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from . import face_recognition_settings
-import os, re, json
+import os
+import re
+import json
 import numpy as np
 import os
 import cv2
@@ -18,76 +20,78 @@ destdir = f'{os.getcwd()}/media'
 def storage(request):
     if request.method == 'POST':
         try:
+            if os.path.exists(f'{destdir}/images_and_vectors.npy'):
+                images_and_vectors = np.load(
+                    f'{destdir}/images_and_vectors.npy', allow_pickle=True)
+            else:
+                images_and_vectors = []
+
             uploaded_file = request.FILES['image']
+            extension = uploaded_file.name.split('.')[-1]
+            if extension != 'jpg' or extension != 'jpeg' or extension != 'png':
+                return HttpResponse(request, status=409)
+
+            tmp_file_name = f"tmp_{uploaded_file.name}." + extension
+
             fs = FileSystemStorage()
-            if not fs.exists(uploaded_file.name):
-                fs.save(uploaded_file.name, uploaded_file)
+            fs.save(tmp_file_name, uploaded_file)
+            target_image = cv2.imread(f'{destdir}/{tmp_file_name}')
+
+            fs.delete(tmp_file_name)
+
+            if face_recognition_settings.add_picture_to_dataset(images_and_vectors, uploaded_file.name, cv2.imread(target_image)):
+                np.save(f'{destdir}/images_and_vectors', images_and_vectors)
+
                 return HttpResponse(request, status=200)
             else:
-                # already exists
-                return HttpResponse(request, status=409)    
+                return HttpResponse(request, status=409)
         except:
             return HttpResponse(request, status=500)
 
-    if request.method == 'GET':
-        try:
-            files = [f for f in next(os.walk(destdir))[2] if f.split('.')[-1] != 'npy']
-            res = []
-            [res.append(filename) for filename in files if re.search(r".jpg$|.jpeg$|.png$", filename)]
-            return HttpResponse([res], status=200)
-        except:
-            return HttpResponse(status=500)
 
 @method_decorator(csrf_exempt)
 def recognize(request):
     if request.method == 'POST':
-        try:                        
-            files = [f for f in next(os.walk(destdir))[2] if f.split('.')[-1] != 'npy']                                  
-            
-            if int(json.loads(request.POST.get('content')).get('count')) <= len(files) and int(json.loads(request.POST.get('content')).get('count')) > 0:                
-                res_len = int(json.loads(request.POST.get('content')).get('count'))
+        try:
+            if !os.path.exists(f'{destdir}/images_and_vectors.npy'):
+                return HttpResponse(request, status=409)
+
+            images_and_vectors = np.load(
+                f'{destdir}/images_and_vectors.npy', allow_pickle=True)
+
+            if int(json.loads(request.POST.get('content')).get('count')) <= len(images_and_vectors) and int(json.loads(request.POST.get('content')).get('count')) > 0:
+                res_len = int(json.loads(
+                    request.POST.get('content')).get('count'))
             else:
-                if len(files) > 10:
+                if len(images_and_vectors) > 10:
                     res_len = 10
                 else:
-                    res_len = len(files)
+                    res_len = len(images_and_vectors)
 
             uploaded_file = request.FILES['image']
+            extension = uploaded_file.name.split('.')[-1]
+            if extension != 'jpg' or extension != 'jpeg' or extension != 'png':
+                return HttpResponse(request, status=409)
 
-            tmp_file_name = "tmp." + uploaded_file.name.split('.')[-1]
+            tmp_file_name = f"tmp_{uploaded_file.name}." + extension
 
-            fs = FileSystemStorage()            
+            fs = FileSystemStorage()
             fs.save(tmp_file_name, uploaded_file)
-            target_image = cv2.imread(f'{destdir}/{tmp_file_name}')                                         
-            
-            face_locations_target, face_encodings_target = face_recognition_settings.get_face_embeddings_from_image(target_image, convert_to_rgb=True)
+            target_image = cv2.imread(f'{destdir}/{tmp_file_name}')
 
-            fs.delete(tmp_file_name)   
+            face_locations_target, face_encodings_target = face_recognition_settings.get_face_embeddings_from_image(
+                target_image, convert_to_rgb=True)
 
-            if json.loads(request.POST.get('content')).get('data'):
-                data = json.loads(request.POST.get('content')).get('data')
+            fs.delete(tmp_file_name)
 
-                tmp = []
-                [tmp.append(f'{filename}') for filename in data if filename in files]
-
-                images = np.array([cv2.imread(f'{destdir}/{filename}') for filename in tmp])
-                images_and_vectors = face_recognition_settings.make_precalculate_work_on_images(images, tmp)               
-
-                if res_len > len(images):
-                    res_len = len(images)
-            else:
-                if os.path.exists(f'{destdir}/images_and_vectors.npy'):
-                    images_and_vectors = np.load(f'{destdir}/images_and_vectors.npy', allow_pickle=True)
-                else:
-                    images = np.array([cv2.imread(f'{destdir}/{filename}') for filename in files if re.search(r".jpg$|.jpeg$|.png$", filename)])
-                    images_and_vectors = face_recognition_settings.make_precalculate_work_on_images(images, files)                                                            
-            
-            nearest_images_with_coefs = face_recognition_settings.python_work(images_and_vectors, face_encodings_target, files, res_len)
+            nearest_images_with_coefs = face_recognition_settings.python_work(
+                images_and_vectors, face_encodings_target, res_len)
 
             tmp = []
             for key in nearest_images_with_coefs:
                 tmp.append(round(nearest_images_with_coefs[key], 2))
-            nearest_images_with_coefs = sorted(nearest_images_with_coefs.items(), key=lambda x: x[1])
+            nearest_images_with_coefs = sorted(
+                nearest_images_with_coefs.items(), key=lambda x: x[1])
 
             interval_end = max(tmp)
             amount_of_layers = 10
@@ -103,26 +107,12 @@ def recognize(request):
                 while distance > left_corner + step:
                 left_corner += step
                 procents.append(amount_of_layers)
-            
+
             result = []
             i = 0
             for key in nearest_images_with_coefs:
-                result.append({"path": key[0], "result": f'{str(procents[i] * 10)}%'})
-                i += 1          
-                    
+                result.append(
+                    {"path": key[0], "result": f'{str(procents[i] * 10)}%'})
+                i += 1
+
             return JsonResponse(res, safe=False, status=200)
-
-
-def precalculate(request):
-    if request.method == 'GET':
-        try :
-            files = [f for f in next(os.walk(destdir))[2] if f.split('.')[-1] != 'npy']
-
-            images = np.array([cv2.imread(f'{destdir}/{filename}') for filename in files if re.search(r".jpg$|.jpeg$|.png$", filename)])
-            images_and_vectors = face_recognition_settings.make_precalculate_work_on_images(images, files)
-
-            np.save(f'{destdir}/images_and_vectors', images_and_vectors)
-
-            return HttpResponse(status=200)
-        except:
-            return HttpResponse(request, status=500)
